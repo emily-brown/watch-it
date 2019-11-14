7d440cf6b5ce04f0a050b442147aa11253700b51
Moumix <mouna.aitichou@gmail.com>
2019-11-14 18:25:34 -0500

    added error validations for register and login

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -2,11 +2,58 @@ import React from "react";
 
 
 export class Login extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+  state = { username : "", password : "", errors: []};
+  
+
+showValidationErr(elm, msg) {
+  this.setState((prevState) => ({ errors: [...prevState.errors, { elm, msg }] } ));
+}
+
+clearValidationErr(elm){
+this.setState((prevState) => {
+  let newArr = [];
+  for(let err of prevState.errors) {
+if(elm != err.elm) {
+newArr.push(err);
+}
+}
+return {errors: newArr};
+});
+}
+
+onUsernameChange(e) {
+this.setState({ username: e.target.value });
+this.clearValidationErr("username");
+}
+
+onPasswordChange(e) {
+  this.setState({ password: e.target.value });
+  this.clearValidationErr("password");
+}
+
+submitLogin(e) {
+
+if(this.state.username == ""){
+ this.showValidationErr("username", "Username is required!");
+
+} if (this.state.password == ""){
+  this.showValidationErr("password", "Password Cannot be empty!");
+}
+
+}
+  
 
   render() {
+
+    let usernameErr = null, passwordErr = null, emailErr = null;
+
+    for(let err of this.state.errors) {
+      if(err.elm == "username"){
+        usernameErr = err.msg;
+      } if (err.elm == "password"){
+        passwordErr = err.msg;
+      }
+    }
     return (
       <div className="base-container" ref={this.props.containerRef}>
         <div className="header">Login</div>
@@ -14,18 +61,27 @@ export class Login extends React.Component {
           <div className="form">
             <div className="form-group">
               <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" />
+              <input type="text" name="username" placeholder="username" 
+              onChange={this.onUsernameChange.bind(this)}
+              />
+              <small className= "danger-error">{usernameErr ? usernameErr : "" }</small>
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" placeholder="password" />
+              <input type="password" name="password" placeholder="password"
+              onChange=
+              {this.onPasswordChange.bind(this)} 
+              />
+              <small className= "danger-error">{passwordErr ? passwordErr : "" }</small>
             </div>
           </div>
         </div>
         <div className="footer">
-          <button type="button" className="btn">
-            Login
-          </button>
+          <button 
+          type="button"
+          className="btn"
+          onClick= 
+          {this.submitLogin.bind(this)}>Login</button>
         </div>
       </div>
     );
